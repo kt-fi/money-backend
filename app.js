72f2b0aed83d110bfaac89e00e7b6e123ce4d4c3
Christopher Hillman <katie5five.5@gmail.com>
2022-12-13 20:17:23 +0100

    Basic App Setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// APP SETUP
+const app = express();
+
+// MIDDLEWARES
+app.use(cors());
+app.use(express.json())
+
+// MONGOOSE SETUP
+mongoose.set('strictQuery', true)
+mongoose.connect('mongodb+srv://money-control:sonicrangers2022@cluster0.02npzs8.mongodb.net/?retryWrites=true&w=majority',
+console.log('DB ONLINE'));
+
+
+
+// START SERVER
+app.listen('3000', console.log('SERVER STARTED'))
